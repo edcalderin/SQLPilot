914b35be8c705aad2d9d3b97ae21d2aa82b74a5a
Erick <edcm.erick@gmail.com>
2025-02-15 15:55:34 -0500

    ✨ feat: Writing data using Snowflake Session

--- src/connection.py
+++ src/connection.py
@@ -1,15 +1,22 @@
-import snowflake.connector as sc
+from snowflake.snowpark.session import Session
+import pandas as pd
 from dotenv import load_dotenv
 from pathlib import Path
-import os 
+import os
+from llama_index.core.settings import Settings
+from llama_index.llms.openai import OpenAI
+from llama_index.core import SQLDatabase
+from llama_index.core.query_engine import NLSQLTableQueryEngine
+from sqlalchemy import create_engine
 
 env_path = Path(__file__).parent.parent / ".env"
 load_dotenv(dotenv_path=env_path, override=True)
 
 class SnowflakeConnection:
-    @staticmethod    
-    def create_connection():
-        con = sc.connect(
+
+    @staticmethod
+    def _credentials()->dict:
+        return dict(
             user=os.getenv("USER"),
             password=os.getenv("PASSWORD"),
             account=os.getenv("ACCOUNT"),
@@ -17,5 +24,27 @@ class SnowflakeConnection:
             schema=os.getenv("SCHEMA"),
             role=os.getenv("ROLE_NAME")
         )
-        return con
-            
+
+    def _create_session(self)->Session:
+        return Session.builder.configs(self._credentials()).create()
+    
+    def data_loading(self, file):
+        df: pd.DataFrame = pd.read_csv(file)
+        session: Session = self._create_session()
+        snowpark_df = session.write_pandas(df,table_name=os.getenv("TABLE_NAME"),database=os.getenv("DATABASE_NAME"),schema=os.getenv("SCHEMA"),quote_identifiers=True,auto_create_table=True, overwrite=True)
+        return snowpark_df
+
+    def data_querying(self):
+        credentials: dict = self._credentials()
+        snowflake_uri: str = f"snoeflake://{credentials.get('USERNAME')}:{credentials.get('PASSWORD')}@{credentials.get('ACCOUNT')}/{credentials.get('SCHEMA')}?warehouse={credentials.get('WAREHOUSE')}&role={credentials.get('ROLE')}"
+
+        Settings.llm = OpenAI(model="gpt-4o", temperature=0)
+        Settings.chunk_size=1024
+
+        engine = create_engine(snowflake_uri)
+        sql_database = SQLDatabase(engine)
+        query_engine = NLSQLTableQueryEngine(
+            sql_database=sql_database,
+            tables=[credentials.get("TABLE_NAME")],
+        )
+
--- src/main.py
+++ src/main.py
@@ -1,33 +1,9 @@
-import pandas as pd
 import streamlit as st
 from src.connection import SnowflakeConnection
-from snowflake.connector.pandas_tools import write_pandas
 
-snowflake_connection = SnowflakeConnection.create_connection()
-
-def data_loading(file) -> pd.DataFrame:
-    """Function to load data
-
-    Args:
-        file: Data
-
-    Returns:
-        DataFrame
-    """
-    file_df = pd.read_csv(file)
-    snowparkDf = write_pandas(
-        df=file_df,
-        conn=snowflake_connection,
-        table_name="ORGANIZATIONS",
-        database="SQLPILOT",
-        schema="PUBLIC",
-        quote_identifiers=True,
-        auto_create_table=True,
-        overwrite=True
-    )
-    return snowparkDf
-
+snowflake_connection = SnowflakeConnection()
 
 file = st.file_uploader("Upload your CSV file here", type={"csv"})
 if file is not None:
-    df = data_loading(file)
+    df = snowflake_connection.data_loading(file)
+    print("Data successfully saved!")
